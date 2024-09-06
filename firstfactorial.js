8f7ae0a9bec8c2f9d7934cc060e95f7e73dd0a9e
Afune <68373983+Afunesimader@users.noreply.github.com>
2024-09-06 12:10:02 +0300

    Add files via upload
    First Factorial Hide Question Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For the test cases, the range will be between 1 and 18 and the numbers must be integers

---
+++ firstfactorial.js
@@ -0,0 +1,14 @@
+function factorialilze(num){
+    if (num <0 )
+        return -1;
+
+    else if (num == 0)
+        return 1;
+
+    else
+return (num* factorialilze(num-1));
+}
+
+factorialilze(60);
+
+
